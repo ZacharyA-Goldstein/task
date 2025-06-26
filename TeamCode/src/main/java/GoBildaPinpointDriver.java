c2cf70e01ab1dad3900a9edd398e19cbcb20f6aa
ZacharyA-Goldstein <zgoldstein27@klschools.org>
2025-06-26 13:57:48 -0400

    hi marc

--- TeamCode/src/main/java/GoBildaPinpointDriver.java
+++ TeamCode/src/main/java/GoBildaPinpointDriver.java
@@ -46,6 +46,7 @@ import java.util.Arrays;
         description ="goBILDA® Pinpoint Odometry Computer (IMU Sensor Fusion for 2 Wheel Odometry)"
         )
 
+
 public class GoBildaPinpointDriver extends I2cDeviceSynchDevice<I2cDeviceSynchSimple> {
 
     private int deviceStatus   = 0;
